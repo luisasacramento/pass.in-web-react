02c43db65da26abfb82b82b1451ee387a29f35e0
Luisa Santos <luisacristina.sn@icloud.com>
2024-04-03 22:07:59 -0300

    componentes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,16 @@
 // jsx = JavaScrpit XML
 // tsx = TypeScript XML
 import icon from '../assets/icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header(){
 
     return(
         <div className="flex items-center gap-5 py-2">
             <img src={icon}/>
-
-            <nav className="flex items-center gap-5">
-                <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-                <a href="" className="font-medium text-sm">Participantes</a>
+             <nav className='flex items-center gap-5'>
+            <NavLink href="/eventos">Eventos</NavLink>
+            <NavLink href="/participantes">Participantes</NavLink>
             </nav>
        
        </div>
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,11 @@
+interface NavLinkProps {
+    children: string
+    href: string
+}
+
+export function NavLink(props :NavLinkProps){
+    
+    return(
+        <a href={props.href} className="font-medium text-sm">{props.children}</a>
+    )
+}
