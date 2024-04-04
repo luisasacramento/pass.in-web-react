e81c0e84441d11542add212905f259fa92567e81
Luisa Santos <luisacristina.sn@icloud.com>
2024-04-04 11:25:10 -0300

    paginação

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -23,9 +23,28 @@ dayjs.extend(relativeTime)
 export function AttendeeList() {
 
   const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+
+  const totalPages = Math.ceil(attendees.length/10)
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
     setSearch(event.target.value) 
+  }
+
+  function goToFirstPage(){
+    setPage(1)
+  }
+
+  function goToPreviousPage(){
+    setPage(page - 1)
+  }
+
+  function goToNextPage(){
+    setPage(page + 1)
+  }
+
+  function goToLastPage(){
+    setPage(totalPages)
   }
 
 
@@ -60,7 +79,7 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {attendees.map((attendee) => {
+          {attendees.slice((page - 1 )* 10, page * 10).map((attendee) => {
             return (
               <TableRow key={attendee.id}>
                 <TableCell>
@@ -86,24 +105,24 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell colSpan={3}>Mostrando 10 de {attendees.length} itens</TableCell>
             <td
               className="py-3 px-4 text-sm text-zinc-300 text-right"
               colSpan={3}
             >
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>Página {page} de {totalPages}</span>
                 <div className="flex gap-1.5">
-                  <IconButton transparent={false}>
+                  <IconButton transparent={false} onClick={goToFirstPage} disabled={ page== 1 }>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton transparent={false}>
+                  <IconButton transparent={false} onClick={goToPreviousPage} disabled={ page== 1 }>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton transparent={false}>
+                  <IconButton transparent={false} onClick={goToNextPage} disabled={ page== totalPages}>
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton transparent={false}>
+                  <IconButton transparent={false} onClick={goToLastPage} disabled={ page== totalPages}>
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
