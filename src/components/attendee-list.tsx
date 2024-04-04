a1a71f7da07b68bd2853835e985afa1eae3b60f5
Luisa Santos <luisacristina.sn@icloud.com>
2024-04-04 11:05:36 -0300

    lista

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,24 +6,41 @@ import {
   ChevronRight,
   ChevronsRight,
 } from "lucide-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { TableRow } from "./table/table.row";
+import { ChangeEvent, useState } from "react";
+import { attendees } from "../data/attendees";
+
+dayjs.extend(relativeTime)
+
+
 
 export function AttendeeList() {
+
+  const [search, setSearch] = useState('')
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
+    setSearch(event.target.value) 
+  }
+
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
-          <input
+          <input onChange={onSearchInputChanged}
             className="bg-transparent felx-1 outline-none border-0 p-0 text-sm"
             placeholder="Buscar participante..."
           />
         </div>
+        {search}
       </div>
 
       <Table>
@@ -43,29 +60,21 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.map((attendee) => {
             return (
-              <TableRow
-                key={i}
-                className="border-b border-white/10 hover:bg-white/5 "
-              >
+              <TableRow key={attendee.id}>
                 <TableCell>
-                  <input
-                    type="checkbox"
-                    className="size-4 bg-black/20 rounded-md border-white/10"
-                  />
+                  <input type="checkbox" className="size-4 bg-black/20 rounded-md border-white/10"/>
                 </TableCell>
-                <TableCell>12383</TableCell>
+                <TableCell>{attendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
-                    <span className="font-semibold text-white">
-                      Luisa dos Santos Neves
-                    </span>
-                    <span>luisacristina.sn@icloud.com</span>
+                    <span className="font-semibold text-white">{attendee.name}</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 dias atrás</TableCell>
-                <TableCell>3 dias atrás</TableCell>
+                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
